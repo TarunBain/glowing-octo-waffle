c72dd167d5c0420569255e185a3eefdfa758c77f
Dell <Dell@DELL-PC>
2021-01-04 20:48:57 +0530

    square a number

--- commit_and_push/src/commit_and_push/Test.java
+++ commit_and_push/src/commit_and_push/Test.java
@@ -1,10 +1,19 @@
 package commit_and_push;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hey there!");
-
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Enter a number");
+			int num = sc.nextInt();
+			System.out.println("Squere is "+ num * num);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
